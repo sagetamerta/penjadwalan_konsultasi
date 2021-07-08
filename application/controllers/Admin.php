c2281c460e28dff76cca87cc67b5d1dc7bcb1f4a
Saget Amerta <sagetamerta@gmail.com>
2021-07-08 14:50:02 +0800

    Admin changeAccess move to Admin_model

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -82,21 +82,7 @@ class Admin extends CI_Controller
 
     public function changeAccess()
     {
-        $menu_id = $this->input->post('menuId');
-        $role_id = $this->input->post('roleId');
-
-        $data = [
-            'role_id' => $role_id,
-            'menu_id' => $menu_id
-        ];
-
-        $result = $this->db->get_where('user_access_menu', $data);
-
-        if ($result->num_rows() < 1) {
-            $this->db->insert('user_access_menu', $data);
-        } else {
-            $this->db->delete('user_access_menu', $data);
-        }
+        $this->Admin_model->changeAccess();
 
         $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
         Access Changed!</div>');
--- application/models/Admin_model.php
+++ application/models/Admin_model.php
@@ -29,4 +29,23 @@ class Admin_model extends CI_Model
         $this->db->where('id', $role_id);
         $this->db->delete('user_role');
     }
+
+    public function changeAccess()
+    {
+        $menu_id = $this->input->post('menuId');
+        $role_id = $this->input->post('roleId');
+
+        $data = [
+            'role_id' => $role_id,
+            'menu_id' => $menu_id
+        ];
+
+        $result = $this->db->get_where('user_access_menu', $data);
+
+        if ($result->num_rows() < 1) {
+            $this->db->insert('user_access_menu', $data);
+        } else {
+            $this->db->delete('user_access_menu', $data);
+        }
+    }
 }
