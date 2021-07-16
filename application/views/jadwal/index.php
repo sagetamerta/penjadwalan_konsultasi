de747d0f8c5e12a4b73f2fb6266e1174ee92676f
Saget Amerta <sagetamerta@gmail.com>
2021-07-16 21:58:12 +0800

    Take input with javascript

--- application/views/jadwal/index.php
+++ application/views/jadwal/index.php
@@ -13,36 +13,51 @@
             <form action="<?= base_url('psikolog/addjadwal'); ?>" method="post">
                 <div class="form-group">
                     <label for="popsize">Population Size</label>
-                    <input type="number" class="form-control" name="popsize" id="popsize" placeholder="ex : 10" value="10">
+                    <input type="number" class="form-control" name="popsize" id="popsize" placeholder="ex : 10">
                     <small>Banyak populasi yang akan dibuat (popsize)</small>
                 </div>
                 <div class="form-group">
                     <label for="cr">Crossover Rate</label>
-                    <input type="number" class="form-control" name="cr" id="cr" step="0.01" placeholder="ex : 0.5" value="0.5">
+                    <input type="number" class="form-control" name="cr" id="cr" step="0.01" placeholder="ex : 0.5">
                     <small>Nilai untuk persilangan (crossover)</small>
                 </div>
                 <div class="form-group">
                     <label for="mr">Mutation Rate</label>
-                    <input type="number" class="form-control" name="mr" id="mr" step="0.01" placeholder="ex : 0.5" value="0.5">
+                    <input type="number" class="form-control" name="mr" id="mr" step="0.01" placeholder="ex : 0.5">
                     <small>Nilai untuk mutasi (mutation)</small>
 
                 </div>
                 <div class="form-group">
                     <label for="iterasi">Iterasi</label>
-                    <input type="number" class="form-control" name="iterasi" id="iterasi" placeholder="ex : 1000" value="1000">
+                    <input type="number" class="form-control" name="iterasi" id="iterasi" placeholder="ex : 1000">
                     <small>Seberapa banyak melakukan perulangan sebelum mencapai nilai fitness yang diinginkan</small>
 
                 </div>
                 <div class="form-group">
                     <label for="thresholdSaget"> Nilai Fitness Minimal</label>
-                    <input type="number" class="form-control" name="thresholdSaget" id="thresholdSaget" placeholder="ex : 0.07" step="0.01" value="0.07">
+                    <input type="number" class="form-control" name="thresholdSaget" id="thresholdSaget" placeholder="ex : 0.07" step="0.01">
                     <small>Batas untuk menentukan minimal kualitas dari individu</small>
 
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="button" onclick="getData()" class="btn btn-primary">Submit</button>
             </form>
 
         </div>
     </div>
 
-    <script src="<?= base_url(); ?>assets/js/algoritma.js"></script>
+    <!-- <script src="<?= base_url(); ?>assets/js/algoritma.js"></script> -->
+    <script type="text/javascript">
+        var popsize;
+        var cr;
+        var mr;
+        var iterasi;
+        var thresholdSaget;
+
+        function getData() {
+            popsize = document.getElementById("popsize").value;
+            cr = document.getElementById("cr").value;
+            mr = document.getElementById("mr").value;
+            iterasi = document.getElementById("iterasi").value;
+            thresholdSaget = document.getElementById("thresholdSaget").value;
+        }
+    </script>
