e9d443cea919610f730a9247c91fb9c536d919ea
Saget Amerta <sagetamerta@gmail.com>
2021-07-28 22:55:31 +0800

    Hari dan jadwal working!

--- application/controllers/Jadwal.php
+++ application/controllers/Jadwal.php
@@ -31,62 +31,27 @@ class Jadwal extends CI_Controller
             $this->load->view('jadwal/add', $data);
             $this->load->view('templates/footer');
         } else {
-            //ambil value dari inputan
-            $jadwalTerbaik = $this->input->post('jadwalTerbaik'); //string
-            $kode_jadwal = $this->input->post('kode_jadwal'); //string
-            $banyak_per_sesi = $this->input->post('banyak_per_sesi'); //string
 
-            //convert to array int
-            $jadwal = array_map('intval', explode(',', $jadwalTerbaik));
+            $kode_jadwal = $this->input->post('kode_jadwal');
+            $banyak_per_sesi = $this->input->post('banyak_per_sesi'); //harusnya nilainya 3 ya buat testing
+            $jadwalterbaik = $this->input->post('jadwalTerbaik'); //string => ubah jadi array int
 
-            //ambil hari
-            $hari = $this->db->select('id_hari')->get('hari')->result_array();
-            $data_hari = [];
-            foreach ($hari as $h) {
-                $data_hari[] = $h['id_hari'];
+            $newjadwal = array_map('intval', explode(',', $jadwalterbaik)); //array int
+            $jadwal_per_hari = array_chunk($newjadwal, $banyak_per_sesi); //array dibagi seberapa banyak psikolog perhari yg akan dibagi menjadi 3 sesi
+
+            for ($i = 0; $i < count($jadwal_per_hari); $i++) {
+                echo "Hari ke-" . ($i + 1) . "  " . json_encode($jadwal_per_hari[$i]) . "<br>";
+                $sesi = array_chunk($jadwal_per_hari[$i], 2);
+                for ($j = 0; $j < count($sesi); $j++) {
+                    echo "Sesi ke-" . ($j + 1) . " " . json_encode($sesi[$j]) . "<br>";
+                }
             }
-            $str_hari = implode(',', $data_hari);
-            //new hari
-            $new_hari = array_map('intval', explode(',', $str_hari));
-
-            //ambil sesi
-            $sesi = $this->db->select('id_sesi')->get('sesi')->result_array();
-            $data_sesi = [];
-            foreach ($sesi as $s) {
-                $data_sesi[] = $s['id_sesi'];
-            }
-            $str_sesi = implode(',', $data_sesi);
-            //new sesi
-            $new_sesi = array_map('intval', explode(',', $str_sesi));
-
-            echo count($jadwal) . "| JADWAL | " . json_encode($jadwal) . "<br>";
-            echo count($hari) . "| HARI | " . json_encode($new_hari) . "<br>";
-            echo count($sesi) . "| SESI | " . json_encode($new_sesi) . "<br>";
-
-
-            //buat for untuk insert ke table jadwal dengan urutan
-            // kode_jadwal | id_hari | id_sesi | id_psikolog
-
-            // kode_jadwal | id_hari | id_sesi | id_psikolog
-            // J01          | 1     | 1        | 6
-            // J01          | 1     | 1        | 13
-            // J01          | 1     | 1        | 15
-            // J01          | 1     | 1        | 1
-            // J01          | 1     | 1        | 7
-            // J01          | 1     | 2        | 18
-            // J01          | 1     | 2        | 20
-            // J01          | 1     | 2        | 4
-            // J01          | 1     | 2        | 29
-            // J01          | 1     | 2        | 35
-
 
             die;
             //insert data ke table db sebanyak jadwal (id_psikolog)
             $data = array(
                 'kode_jadwal' => $kode_jadwal,
-                'id_hari' => $hari,
-                'id_sesi' => $sesi,
-                // 'id_psikolog' => $id_psikolog
+                'kode_jadwal' => $kode_jadwal,
             );
             $this->db->insert('psikolog', $data);
         }
