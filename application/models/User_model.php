fbe2b3a38c087490e7397d76441f4013611d3874
Saget Amerta <sagetamerta@gmail.com>
2021-08-05 17:36:13 +0800

    Update user model and controller

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -8,4 +8,68 @@ class User_model extends CI_Model
     {
         return $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
     }
+
+    public function editUser()
+    {
+        $name = $this->input->post('name');
+        $email = $this->input->post('email');
+
+        $data['user'] = $this->user();
+
+        //cek jika ada gambar yang akan diupload
+        $upload_image = $_FILES['image']['name'];
+
+        if ($upload_image) {
+            $config['upload_path'] = './assets/img/profile/';
+            $config['allowed_types'] = 'gif|jpg|png';
+            $config['max_size']     = '2048';
+
+            $this->load->library('upload', $config);
+
+            if ($this->upload->do_upload('image')) {
+                $old_image = $data['user']['image'];
+                if ($old_image != 'default.jpg') {
+                    unlink(FCPATH . 'assets/img/profile/' . $old_image);
+                }
+                $new_image = $this->upload->data('file_name');
+                $this->db->set('image', $new_image);
+            } else {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">' . $this->upload->display_errors() . '</div>');
+                redirect('user');
+            }
+        }
+        $this->db->set('name', $name);
+        $this->db->where('email', $email);
+        $this->db->update('user');
+    }
+
+    public function changePasswordUser()
+    {
+        $data['user'] = $this->user();
+        $current_password = $this->input->post('current_password');
+        $new_password = $this->input->post('new_password1');
+
+        if (!password_verify($current_password, $data['user']['password'])) {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                Wrong current Password!</div>');
+            redirect('user/changepassword');
+        } else {
+            if ($current_password == $new_password) {
+                $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert">
+                New password cannot be the same as current password!</div>');
+                redirect('user/changepassword');
+            } else {
+                // password sudah ok
+                $password_hash = password_hash($new_password, PASSWORD_DEFAULT);
+
+                $this->db->set('password', $password_hash);
+                $this->db->where('email', $this->session->userdata('email'));
+                $this->db->update('user');
+
+                $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">
+                Password changed!</div>');
+                redirect('user/changepassword');
+            }
+        }
+    }
 }
--- application/views/auth/blocked.php
+++ application/views/auth/blocked.php
@@ -2,89 +2,29 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>403 Access Forbidden!</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>403 | Access Forbidden</title>
     <style>
-        @import url('https://fonts.googleapis.com/css?family=Press+Start+2P');
-
-        html,
-        body {
-            width: 100%;
+        #main {
             height: 100vh;
-            margin: 0;
-        }
-
-        * {
-            font-family: 'Press Start 2P', cursive;
-            box-sizing: border-box;
-        }
-
-        #app {
-            padding: 1rem;
-            background: black;
-            display: flex;
-            height: 100%;
-            justify-content: center;
-            align-items: center;
-            color: #54FE55;
-            text-shadow: 0px 0px 10px;
-            font-size: 6rem;
-            flex-direction: column;
-
-        }
-
-        .txt {
-            font-size: 1.8rem;
-        }
-
-        @keyframes blink {
-            0% {
-                opacity: 0
-            }
-
-            49% {
-                opacity: 0
-            }
-
-            50% {
-                opacity: 1
-            }
-
-            100% {
-                opacity: 1
-            }
-        }
-
-        a {
-            text-decoration: none;
-            margin-top: 25px;
-            font-size: 25px;
-            color: #00FF41;
-        }
-
-        .blink {
-            animation-name: blink;
-            animation-duration: 1s;
-            animation-iteration-count: infinite;
         }
     </style>
 </head>
 
 <body>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
 
-    <div id="app">
-        <div>403</div>
-        <div class="txt">
-            Access Forbidden<span class="blink">_</span>
+    <div class="d-flex justify-content-center align-items-center" id="main">
+        <h1 class="mr-3 pr-3 align-top border-right inline-block align-content-center">404</h1>
+        <div class="inline-block align-middle">
+            <h2 class="font-weight-normal lead" id="desc">Access Forbidden.</h2>
+            <a href="<?= base_url(''); ?>">Go back</a>
         </div>
-        <a class="link" href="<?= base_url(); ?>">Go back!</a>
     </div>
 </body>
 
